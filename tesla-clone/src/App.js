6a5716f38daa3ae67cd5ca9873eeaa8b2a80445f
Vukipoo <perovicv515@gmail.com>
2023-05-27 17:02:39 -0700

    add menu sidebar

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -8,8 +8,7 @@ import ModelX from './components/ModelX'
 import SolarPanels from './components/SolarPanels'
 import SolarRoof from './components/SolarRoof'
 import Accessories from './components/Accessories'
-
-
+import Menu from './components/Menu'
 
 
 function App() {
@@ -20,8 +19,9 @@ function App() {
       <div className='home-n-header'>
        <Header />
       <Home />
+      <Menu />
       </div>
-      
+     
       <ModelS />
       <ModelY />
       <Model3 />
@@ -38,4 +38,14 @@ export default App;
 
 //todo
 
-//create secitons for rest of products
+//menu thing popup sidebar  figure out how to use the state for this
+//arraw that points down 
+
+
+
+
+//thoughts
+
+//100% sure there is a way more efficient way to handle the sections and the styling for them so if event fux are given do that   PROPS PROPS PROPS
+//check out react reveal for the text appeariing nice animation wowow so nice animation 
+//lets add the front page gif thing that the actual tesla website has going on
